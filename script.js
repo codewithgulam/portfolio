374b27a38318a7fffccc71d15c6bb713414b1046
unknown <dashcroc2601@gmail.com>
2021-10-06 18:34:30 +0530

    Added cards for skills

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const b2t = ScrollReveal({
     reset: true
 })
 
-b2t.reveal('.devicon-python-plain-wordmark')
-b2t.reveal('.devicon-java-plain-wordmark',{delay:100})
-b2t.reveal('.devicon-cplusplus-line-wordmark',{delay:300})
-b2t.reveal('.devicon-javascript-plain',{delay:500})
+b2t.reveal('.container-python')
+b2t.reveal('.container-java',{delay:100})
+b2t.reveal('.container-cpp',{delay:300})
+b2t.reveal('.container-js',{delay:500})
 b2t.reveal('.grid-item',{delay:250})
